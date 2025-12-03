f47e3505713183ae894caea127172f6151870ede
sagar5412 <sagar.gk.bly@gmail.com>
2025-12-03 19:40:34 +0530

    fixed rectangle issues

--- app/components/canvas/Canvas.tsx
+++ app/components/canvas/Canvas.tsx
@@ -2,9 +2,12 @@
 import { useEffect, useRef, useState } from "react";
 import { Shape } from "@/app/types/Shapes";
 
+type selectedShapes = "rectangle";
+
 export function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [shapes, setShapes] = useState<Shape[]>([]);
+  const [selectedTool, setSelectedTool] = useState<selectedShapes>("rectangle");
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) {
@@ -28,18 +31,51 @@ export function Canvas() {
   const handleMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
     const startX = e.clientX;
     const startY = e.clientY;
+    let currentShape: Shape | null = null;
     const handleMouseMove = (e: MouseEvent) => {
       const width = e.clientX - startX;
       const height = e.clientY - startY;
+      currentShape = {
+        type: selectedTool,
+        x: startX,
+        y: startY,
+        width,
+        height,
+      };
 
-      setShapes((prevShapes) => [
-        ...prevShapes,
-        { type: "rectangle", x: startX, y: startY, width, height },
-      ]);
+      const canvas = canvasRef.current;
+      if (!canvas) {
+        return;
+      }
+      const ctx = canvas.getContext("2d");
+      if (!ctx) {
+        return;
+      }
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      shapes.forEach((shape) => {
+        if (shape.type === "rectangle") {
+          ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+        }
+      });
+
+      if (currentShape) {
+        if (currentShape.type === "rectangle") {
+          ctx.strokeRect(
+            currentShape.x,
+            currentShape.y,
+            currentShape.width,
+            currentShape.height
+          );
+        }
+      }
     };
     const handleMouseUp = () => {
       document.removeEventListener("mousemove", handleMouseMove);
       document.removeEventListener("mouseup", handleMouseUp);
+      if (currentShape) {
+        const shapeToAdd = currentShape;
+        setShapes((prevShapes) => [...prevShapes, shapeToAdd]);
+      }
     };
     document.addEventListener("mousemove", handleMouseMove);
     document.addEventListener("mouseup", handleMouseUp);
